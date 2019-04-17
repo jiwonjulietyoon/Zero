76a73ad6dd795b0a799b0884b425842565ca90a0
Jiwon <jiwonjulietyoon@gmail.com>
2019-04-17 15:10:39 +0900

    Designate root page; Add login requirements to todos:list

--- Zero/urls.py
+++ Zero/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from todos import views as todos_views
 
 # MEDIA 관련 import
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', todos_views.list, name="root"),
     path('admin/', admin.site.urls),
     path('todos/', include('todos.urls')),
     path('accounts/', include('accounts.urls')),
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from IPython import embed
 
 # Create your views here.
+
 def list(request):
-    return render(request, 'todos/list.html')
+    if request.user.is_authenticated:   # 로그인된 유저만 메인 페이지로 이동
+        return render(request, 'todos/list.html')
+    else: # 로그인 안 된 사용자는 로그인 페이지로 이동
+        return redirect('accounts:login_signup')
+
