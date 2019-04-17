39ae48c49b712b31f9f31357ef1f497aa9a2f1ae
ChangmoKang <dkssudckdah@gmail.com>
2019-04-17 15:26:13 +0900

    Create 'Label' model

--- todos/models.py
+++ todos/models.py
@@ -1,14 +1,21 @@
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 # Create your models here.
 class Card(models.Model):
     title = models.CharField(default='', max_length=255)
     content = models.TextField(default='')
     image = models.ImageField(blank=True, upload_to='cards/media')
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} // created_at:{self.created_at} // updated_at:{self.updated_at}"
+
+
+class Label(TimeStampedModel):
+    name = models.CharField(blank=True, max_length=50)
+    color = models.IntegerField(default=1, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
